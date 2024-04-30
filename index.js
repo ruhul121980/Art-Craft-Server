1e6affa883ecfeb335e1c593906c5da58e29b736
unknown <ruhul121980@gmail.com>
2024-04-30 23:06:53 +0600

    after deployment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express=require('express');
 const cors=require('cors');
+app.use(cors());
+const express=require('express');
+
 require('dotenv').config()
 const app=express();
 const port=process.env.port || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v1zto12.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
